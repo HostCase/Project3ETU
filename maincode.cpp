24d3d2f3b5b7588007d8081c83619189b1f01f74
HostCase <430ap430@gmail.com>
2023-12-19 17:22:20 +0300

    kakoi-tokall

--- maincode.cpp
+++ maincode.cpp
@@ -1,39 +1,46 @@
 #include <iostream>
 #include <Windows.h>
-#include <time.h>
-
 using namespace std;
 
 
 int main() {
     // const from cin??
-    const int n = 8;
+    const int n = 6;
+    if (n % 2 == 1) {
+        return 0;
+    }
+    
     int arra[n][n];
     int k = 0;
     int value = 1;
-
+    HANDLE hStdout;
+    COORD destCoord;
+    hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
     do{
     for (int x = k; x < n - k; x++) {
-        arra[k][x] = value;
-        cout<<
-        value++;
+       arra[k][x] = 1 + rand() % (n * n);
+       destCoord.X = x * 4;
+       destCoord.Y = k * 2; 
+       SetConsoleCursorPosition(hStdout, destCoord);
+       cout << arra[k][x] << "    ";
+       cout.flush();
     }
-
+    Sleep(3000);
 
     for (int y = k + 1; y < n - k; y++) {
-        arra[y][n - 1 - k] = value;
-        value++;
+        arra[y][n - 1 - k] = 1 + rand() % (n * n);
+
     }
 
 
     for (int x = n - 2 - k; x >= k; x--) {
-        arra[n - 1 - k][x] = value;
+        arra[n - 1 - k][x] = 1 + rand() % (n * n);
         value++;
     }
 
 
     for (int y = n - 2 - k; y > k; y--) {
-        arra[y][k] = value;
+        arra[y][k] = 1 + rand() % (n * n);
         value++;
     }
     k++;
